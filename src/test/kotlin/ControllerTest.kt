65bf2bcc63a2a704436241f8f5f477d85a3b8a86
rmamadgi <rmamadgi@expedia.com>
2020-02-01 18:11:10 -0800

    switching to Kotlin

---
+++ src/test/kotlin/ControllerTest.kt
@@ -0,0 +1,12 @@
+package com.rm.controller
+
+import org.testng.Assert.assertEquals
+import org.testng.annotations.Test
+
+class ControllerTest {
+    @Test
+    fun helloWorld()
+    {
+        assertEquals(true, true)
+    }
+}
